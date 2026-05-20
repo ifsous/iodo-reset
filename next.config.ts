1aba2989f362f757d3f5ec20d2d2934b54eb13fd
ifsous <ivoncrsj@gmail.com>
2026-05-20 10:50:51 -0600

    Align Sentry setup with Next.js guidance

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
   authToken: process.env.SENTRY_AUTH_TOKEN,
-  silent: true,
+  silent: !process.env.CI,
+  tunnelRoute: "/monitoring",
   widenClientFileUpload: true,
 });
